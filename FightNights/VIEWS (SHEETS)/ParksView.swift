8bbe27db6df4aa46e3cbd2751c82a206890ba597
AndreFoulem <timmycasual@icloud.com>
2023-03-30 16:25:28 -0700

    Add the toggle

--- FightNights/VIEWS (SHEETS)/ParksView.swift
+++ FightNights/VIEWS (SHEETS)/ParksView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct ParksView: View {
-   @Environment(\.managedObjectContext) var context
+  //MARK: fetch request
+  @Environment(\.managedObjectContext) var context
+  @FetchRequest<ParkEntity>(sortDescriptors:
+                              [SortDescriptor(\.name_)]) private var parks
   
-  @FetchRequest<ParkEntity>(sortDescriptors: [SortDescriptor(\ParkEntity.name_)])
-    private var parks
+  //MARK: state (local)
+  @State private var sortAscending = true
   
     var body: some View {
       NavigationStack {
@@ -36,6 +39,18 @@ struct ParksView: View {
           .padding(.vertical, 8)
         }//list
         .navigationTitle("Parks")
+        .toolbar {
+          ToolbarItem {
+            Button {
+              sortAscending.toggle()
+              //! using the parks FetchResults sortDescriptors
+              parks.sortDescriptors =
+              [SortDescriptor(\.name_, order: sortAscending ? .forward: .reverse)]
+            } label: {
+              Image(systemName: sortAscending ? "line.3.horizontal.decrease.circle" : "line.3.horizontal.decrease.circle.fill")
+            }
+          }
+        }
       }//ns
       
     }//body
@@ -43,7 +58,7 @@ struct ParksView: View {
 
 struct ParksView_Previews: PreviewProvider {
     static var previews: some View {
-        ParksView()
+      ParksView()
         .environment(\.managedObjectContext, ParksContainer.preview)
     }
 }
