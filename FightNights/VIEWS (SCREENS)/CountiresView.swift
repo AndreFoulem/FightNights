9c1cbf3123a993b3648a4957208053fa8a77280c
AndreFoulem <timmycasual@icloud.com>
2023-03-31 14:16:48 -0700

    Add hasChanges

--- FightNights/VIEWS (SCREENS)/CountiresView.swift
+++ FightNights/VIEWS (SCREENS)/CountiresView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct CountiresView: View {
-  @Environment(\.managedObjectContext) var context
+  @FetchRequest<CountryEntity>(sortDescriptors: []) private var countries
   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      
+      NavigationStack {
+        List(countries) { country in
+          NavigationLink {
+            EditCountry(countryEntity: country)
+          } label : {
+            Text(country.viewName)
+          }
+        }//list
+        .navigationTitle("Countries")
+      }//ns
+      
     }
+}
+
+struct EditCountry: View {
+  let countryEntity: CountryEntity?
+  
+  @Environment(\.managedObjectContext) private var context
+  @Environment(\.dismiss) private var dismiss
+  @State private var country = ""
+  
+  var body: some View {
+    VStack {
+      TextField("country", text: $country)
+        .textFieldStyle(.roundedBorder).padding()
+      Button("Save") {
+        if countryEntity!.name != country {
+          countryEntity!.name = country
+        }
+        if context.hasChanges {
+          print("changed and saved")
+          try? context.save()
+        }
+        dismiss()
+      }
+      .buttonStyle(.borderedProminent)
+      Spacer()
+    }//vs
+    .onAppear {
+      country = countryEntity!.name ?? ""
+    }
+  }
 }
 
 struct CountiresView_Previews: PreviewProvider {
     static var previews: some View {
         CountiresView()
+        .environment(\.managedObjectContext, CountriesContainer.preview)
     }
 }
