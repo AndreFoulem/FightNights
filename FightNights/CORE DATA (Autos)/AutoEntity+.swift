81187ad2c80be2fb6b77e155b1f4d75e6f4b5c1c
AndreFoulem <timmycasual@icloud.com>
2023-04-03 10:15:53 -0700

    Add entity extensions for Core Data entities

---
+++ FightNights/CORE DATA (Autos)/AutoEntity+.swift
@@ -0,0 +1,8 @@
+//
+//  AutoEntity+.swift
+//  FightNights
+//
+//  Created by AndreMacBook on 2023-04-03.
+//
+
+import Foundation
---
+++ FightNights/CORE DATA (Autos)/ManufacturerEntity+.swift
@@ -0,0 +1,8 @@
+//
+//  ManufacturerEntity+.swift
+//  FightNights
+//
+//  Created by AndreMacBook on 2023-04-03.
+//
+
+import Foundation
