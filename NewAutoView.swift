4627532f99e56cf8bc8525d9880d944bbae67b02
AndreFoulem <timmycasual@icloud.com>
2023-04-13 14:07:54 -0700

    Add environment

--- NewAutoView.swift
+++ NewAutoView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct NewAutoView: View {
    var manufacturerEntity: ManufacturerEntity
+  @Environment(\.managedObjectContext) private var context
+  @Environment(\.dismiss) var dismiss
+  
 
     var body: some View {
+      VStack(spacing: 15) {
+        
+      }
     }
 }
 
