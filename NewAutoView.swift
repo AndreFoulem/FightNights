cf7a1a234e454b8ed170a458e10963c50c4389e2
AndreFoulem <timmycasual@icloud.com>
2023-04-13 14:06:49 -0700

    Add manufacturerEntity prop to view init

---
+++ NewAutoView.swift
@@ -0,0 +1,21 @@
+//
+//  NewAutoView.swift
+//  FightNights
+//
+//  Created by AndreMacBook on 2023-04-13.
+//
+
+import SwiftUI
+
+struct NewAutoView: View {
+   var manufacturerEntity: ManufacturerEntity
+
+    var body: some View {
+    }
+}
+
+struct NewAutoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewAutoView()
+    }
+}
