25b9f329c78db144c9c5fc06da01738ac596ed18
AndreFoulem <timmycasual@icloud.com>
2023-04-14 09:43:35 -0700

    Create FamilyContainer file

---
+++ FightNights/CORE DATA (Family)/FamilyContainer.swift
@@ -0,0 +1,15 @@
+//
+//  FamilyContainer.swift
+//  FightNights
+//
+//  Created by AndreMacBook on 2023-04-14.
+//
+
+import CoreData
+
+final class FamilyContainer {
+  
+  let containter: NSPersistentContainer
+  
+  
+}
