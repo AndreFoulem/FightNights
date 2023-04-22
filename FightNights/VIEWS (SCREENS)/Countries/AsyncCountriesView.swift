f77b017349e982f96469dc8eddb98ad70e3ffb5b
AndreFoulem <timmycasual@icloud.com>
2023-04-21 17:38:16 -0700

    Simulate a CoreData error with alert

--- FightNights/VIEWS (SCREENS)/Countries/AsyncCountriesView.swift
+++ FightNights/VIEWS (SCREENS)/Countries/AsyncCountriesView.swift
@@ -30,20 +30,25 @@ struct AsyncCountriesView: View {
               self.error = error
               showError = true
             }
-            
-          }
+          }//task
+        }//button
+        .alert("Error", isPresented: $showError, presenting: error) { _ in
+            //default button
+        } message: { _ in
+          Text("missing text")
         }
-      }
+      }//vs
+    
       
     }//body
     
   func AddNewCountry() async throws {
     try await context.perform {
       let country = CountryEntity(context: context)
-      country.name = "New Country"
+//      country.name = "New Country"
       try context.save() // throws an error
     }
-  }
+   }
   
 }
 
