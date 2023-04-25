9fc14b288f99e4371de16bdc8ef352f2970defde
AndreFoulem <timmycasual@icloud.com>
2023-04-25 15:45:47 -0700

    Add PlanetEntity property viewOrbitalPeriod

--- FightNights/CORE DATA (Planets)/PlanetEntity+.swift
+++ FightNights/CORE DATA (Planets)/PlanetEntity+.swift
@@ -9,16 +9,20 @@ import UIKit
 extension PlanetEntity {
   
   var viewName: String {
-    return name ?? "N/A"
+    return self.name ?? "N/A"
   }
   
   var viewImage: UIImage {
-    if let data = picture,
+    if let data = self.picture,
        let image = UIImage(data: data) {
       return image
     } else {
       return UIImage(systemName: "globe.americas.fill")!
     }
+  }
+  
+  var viewOrbitalPeriod: String {
+    "Orbital Period: \(self.orbitalPeriod)"
   }
 
 }
